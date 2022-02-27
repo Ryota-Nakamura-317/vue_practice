66bec7a8177a53cf7776a89d5f9903e7e7b1f395
中村遼太 <nkmr94317rt@gmail.com>
2022-02-27 20:25:44 +0900

    新規ページの作成とappBar、image配置

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <v-navigation-drawer v-model="drawer" app>
       <v-list-item>
         <v-list-item-content>
-          <!-- v-list-item-subtitleを削除、タイトルをMenuに変更 -->
           <v-list-item-title class="text-h6"> Menu </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
@@ -24,10 +23,23 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar app elevation="0" color="#ffffff">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>小川染色株式会社</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <template v-slot:extension>
+      <v-tabs right hide-slider active-class="font-weight-bold black--text" >
+        <v-tab :ripple="false">小川染色について</v-tab>
+        <v-tab :ripple="false">染色加工</v-tab>
+        <v-tab :ripple="false">設備紹介</v-tab>
+        <v-tab :ripple="false">会社概要</v-tab>
+        <v-tab :ripple="false">お問い合わせ</v-tab>
+        <v-tab :ripple="false">お知らせ</v-tab>
+        <v-tab :ripple="false" to="/shop">色糸パレット</v-tab>
+        <v-tab :ripple="false">するりぃと</v-tab>
+      </v-tabs>
+      </template>
     </v-app-bar>
 
     <v-main>
@@ -72,6 +84,7 @@ export default Vue.extend({
     items: [
       { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
       { title: "About", icon: "mdi-help-box", to: "/about" },
+      { title: "Shop", icon: "mdi-help-box", to: "/shop" },
     ],
   }),
 });
---
+++ src/views/Shop.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+    <v-img
+      :src="require('../assets/h1_pallet.png')"
+      class="mr-16"
+      contain
+      height="200"
+    />
+    <v-spacer></v-spacer>
+  </v-row>
+</template>
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    return;
+  },
+});
+</script>
